bec1aa5d3ab0433550c19654a24800d0ba6eae52
MaxMilshin <maxonchik2003@yandex.ru>
2024-09-28 21:04:28 +0300

    add assign command test

--- cli/src/test/kotlin/AssignCommandTest.kt
+++ cli/src/test/kotlin/AssignCommandTest.kt
@@ -2,19 +2,16 @@ import org.junit.jupiter.api.Assertions.*
 
 class AssignCommandTest {
     @org.junit.jupiter.api.Test
-    fun `test assign command`() { // TODO()
-        assert(true)
-//        val outputBuffer = StringBuilder()
-//        val env = Environment()
-//        val arguments = listOf("stay", "out", "of", "my", "territory")
-//        val command = AssignCommand(
-//            MockIStream(),
-//            MockOStream(),
-//            MockOStream(),
-//            arguments,
-//            env
-//        )
-//        command.execute()
-//        assertEquals("stay out of my territory", outputBuffer.toString())
+    fun `test assign command`() {
+        val env = Environment()
+        val command = AssignCommand(
+            MockIStream(),
+            MockOStream(),
+            MockOStream(),
+            listOf("variableName", "variableValue"),
+            env
+        )
+        command.execute()
+        assertEquals("variableValue", env.getVariable("variableName"))
     }
 }
