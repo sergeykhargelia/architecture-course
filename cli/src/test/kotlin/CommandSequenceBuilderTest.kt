5be261f9bb70eee27010b7a167e911a142a2361b
Sergey Khargelia <sergey.khargelia@gmail.com>
2024-10-05 19:38:55 +0300

    add tests for executor and command sequence builder

--- cli/src/test/kotlin/CommandSequenceBuilderTest.kt
+++ cli/src/test/kotlin/CommandSequenceBuilderTest.kt
@@ -1,30 +1,41 @@
 import org.junit.jupiter.api.Assertions.*
+import kotlin.reflect.KClass
 
 class CommandSequenceBuilderTest {
+    private val env = Environment()
+    private val builder = CommandSequenceBuilder(env)
+
     @org.junit.jupiter.api.Test
-    fun buildCommands() {
-        val env = Environment()
-        val builder = CommandSequenceBuilder(env)
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.Cat, listOf("cat", "example.txt")))).first() is CatCommand
+    fun buildSingleCommand() {
+        // (description, expected type, number of arguments to drop)
+        val commandsParams: List<Triple<CommandDescription, KClass<*>, Int>> = listOf(
+            Triple(CommandDescription(CommandType.Cat, listOf("cat", "example.txt")), CatCommand::class, 1),
+            Triple(CommandDescription(CommandType.Echo, listOf("echo", "example.txt")), EchoCommand::class, 1),
+            Triple(CommandDescription(CommandType.WC, listOf("wc", "example.txt")), WCCommand::class, 1),
+            Triple(CommandDescription(CommandType.PWD, listOf("pwd")), PWDCommand::class, 1),
+            Triple(CommandDescription(CommandType.Exit, listOf("exit")), ExitCommand::class, 1),
+            Triple(CommandDescription(CommandType.Assign, listOf("x", "kek")), AssignCommand::class, 0),
+            Triple(CommandDescription(CommandType.External, listOf("ls", "-lAh")), ExternalCommand::class, 0),
         )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.Echo, listOf("echo", "example.txt")))).first() is EchoCommand
-        )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.WC, listOf("wc", "example.txt")))).first() is WCCommand
-        )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.PWD, listOf("pwd")))).first() is PWDCommand
-        )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.Exit, listOf("exit")))).first() is ExitCommand
-        )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.Assign, listOf("x", "ex")))).first() is AssignCommand
-        )
-        assertTrue(
-            builder.buildCommands(listOf(CommandDescription(CommandType.External, listOf("java", "--version")))).first() is ExternalCommand
-        )
+
+        for ((description, type, argsToDrop) in commandsParams) {
+            val result = builder.buildCommands(listOf(description)).first()
+            assertTrue(type.isInstance(result))
+            assertEquals(description.description.drop(argsToDrop), result.args)
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    fun buildMultipleCommands() {
+        val result = builder.buildCommands(listOf(
+            CommandDescription(CommandType.PWD, listOf("pwd")),
+            CommandDescription(CommandType.Cat, listOf("cat"))
+        ))
+
+        assertTrue(result[0].outputStream == result[1].inputStream)
+        assertTrue(result[0] is PWDCommand)
+        assertEquals(emptyList<String>(), result[0].args)
+        assertTrue(result[1] is CatCommand)
+        assertEquals(emptyList<String>(), result[1].args)
     }
 }
